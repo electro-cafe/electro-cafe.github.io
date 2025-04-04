3f6db437c2b8c5886f989b1af8d5a7089c4a76d2
Thomas Fessler <culiacan77@users.noreply.github.com>
2025-04-04 17:12:33 +0200

    updated arduino rainbow code for 3point switch button use

--- Arduino_code/Neopixel_library_test/Switch_rainbow_red/Switch_rainbow_red.ino
+++ Arduino_code/Neopixel_library_test/Switch_rainbow_red/Switch_rainbow_red.ino
@@ -1,31 +1,45 @@
 #include <Adafruit_NeoPixel.h>
 
 const int kPin = 4; // Pin connected to the LED strip
-const int kNumberOfLed = 2; // Number of LEDs
+const int kNumberOfLed = 1
+; // Number of LEDs
 
-const int kSwitchPin = 47;  // Brown wire (switch signal)
+const int kSwitchPinForward = 45;  // White wire (switch signal)
+const int kSwitchPinBackward = 47;  // Brown wire (switch signal)
 
-Adafruit_NeoPixel pixels(kNumberOfLed, kPin, NEO_GRB + NEO_KHZ800);
+int kUpdateSpeed = 8; //increase for faster rainbow
+uint8_t kPausedHue = 0;  // it will store the hue value when switch is in neutral position
 
-#define DELAYVAL 1000 // Time (in milliseconds) between updates
 
-bool switchState = false;
+Adafruit_NeoPixel pixels(kNumberOfLed, kPin, NEO_GRB + NEO_KHZ800); //create an object (instance of a class) named pixels of type Adafruit_NeoPixel.
+//third argument is the pixel type. You choose it from a list furnished by Adafruit_NeoPixel.h
 
-void setup() {
-  pinMode(kSwitchPin, INPUT_PULLUP);  // Enable internal pull-up resistor
-  Serial.begin(9600); // Start serial communication
+#define DELAYVAL 1000 //the keyword #define is not a type such as int. 
+//It tells to replace every instances of DELAYVAL (a name we choose) by 1000 before the code is compiled. 
+//Advantage: it don't take memory as a variable of type int would.
+
+void setup() { //this is a mandatory function of type void that runs once before loops and set things up
+  if (kUpdateSpeed == 0) { //avoid a division by 0
+  kUpdateSpeed++;
+  
+  }
+  pinMode(kSwitchPinForward, INPUT_PULLUP);  // Enable internal pull-up resistor on given pin. the pin will read HIGH it avoid floating issue.
+  pinMode(kSwitchPinBackward, INPUT_PULLUP);   
+  Serial.begin(9600); // Set baud rate. this allows debugging message to be send to the serial monitor.
   delay(1000); // Give Serial Monitor time to connect
   Serial.println("Starting LED debug...");
   
-  pixels.begin(); // Initialize NeoPixel strip object
-  pixels.clear(); // Clear all pixels initially
-  pixels.show();  // Send update to turn off all LEDs
+  //good practice to run those 3 functions
+  pixels.begin(); // set the pin given in pixels argument as an output.
+  pixels.clear(); // Clear all pixels 
+  pixels.show();  // update the pixel state.
+  InitializePixelRed(); //set pixel red to begin
 }
 
 // Print pixel rgb value for debugging
 void printPixelData() {
   Serial.print("Pixel Data: ");
-  uint8_t* ledData = pixels.getPixels();
+  uint8_t* ledData = pixels.getPixels(); //this function return a pointer of type uint8_t that we named ledData
   
   for (int i = 0; i < kNumberOfLed; i++) {
     int r = ledData[i * 3];     // Red
@@ -43,38 +57,58 @@ void printPixelData() {
 
 // Set all LEDs Red
 void InitializePixelRed() {
-  pixels.setPixelColor(0, pixels.Color(150, 0, 0)); 
-  pixels.setPixelColor(1, pixels.Color(150, 0, 0)); 
+  for(int i = 0; i < kNumberOfLed; i++){ //set each led in the chain to red
+  pixels.setPixelColor(i, pixels.Color(150, 0, 0)); //first argument = number of led in the chain, second argument = rgb value.
+  }
   pixels.show();  // Update LED strip
   printPixelData(); // Debug output
   delay(DELAYVAL);
 }
 
-// Function to display the rainbow effect
+uint16_t getHue() { ///////////////////////pas sur
+  pixels.ColorHSV();
+  return hue;
+}
+
 void showRainbow() {
   for (int i = 0; i < 256; i++) {
-    pixels.rainbow(i); // Start rainbow effect with changing hue
+    pixels.rainbow(kPausedHue + i); // Start rainbow effect based on paused hue. Increase value
+    pixels.show();  // Update LED strip
+    delay(10); // Small delay to slow down the rainbow effect
+  }
+}
+
+
+void showRainbowReverse() {
+  for (int i = 0; i < 256; i++) {
+    pixels.rainbow(kPausedHue - i); // Start rainbow effect based on paused hue. Decrease value
     pixels.show();  // Update LED strip
     delay(10); // Small delay to slow down the rainbow effect
   }
 }
 
 void loop() {  
-  // Read the state of the switch pin (whether it's HIGH or LOW)
-  switchState = digitalRead(kSwitchPin) == HIGH;  // Switch is active (LOW) when pressed
-  static uint16_t first_hue = 0;  // Start hue value
 
-  if (switchState) {
-    // If the switch is in Left or Right position (LOW state)
-    pixels.clear();
-    InitializePixelRed();  // Display Red LED
-  } else {
-    // If the switch is in Center position (HIGH state)
-    pixels.rainbow(first_hue, 1, 255, 255, true);
+const bool switchStateLeft = digitalRead(kSwitchPinForward) == LOW;  // determine the position of the button by reading the state of the pin is LOW when switch connect it to gnd
+const bool switchStateRight = digitalRead(kSwitchPinBackward) == LOW;
+
+
+  if (switchStateLeft) { // If the switch is in Left position kpin 45 reads LOW 
+
+    showRainbow();
     pixels.show();  // Update LED strip
-    first_hue += 256;  // Increase hue for next cycle
-    Serial.println("Rainbow activated");
+    Serial.println("reverse Rainbow activated");
     printPixelData();  // Debug output
-    delay(DELAYVAL/8);
+    delay(DELAYVAL/kUpdateSpeed);
+  } else if (switchStateRight) { // If the switch is in Right position kpin 47 reads LOW
+ 
+    showRainbowReverse();
+    pixels.show();  // Update LED strip
+    Serial.println("reverse Rainbow activated");
+    printPixelData();  // Debug output
+    delay(DELAYVAL/kUpdateSpeed);
+  } else {
+    uint8 kPausedHue = getHue();
+    delay(DELAYVAL); //do nothing
   }
 }
